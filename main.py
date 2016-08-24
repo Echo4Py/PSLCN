aa24dda5eb35a3d93b9e67d89309341694722404
George <babyfor@qq.com>
2016-08-24 15:06:00 +0800

    init virtualenv and install flask

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from jinja2 import Template
+import time
+from filecmp import cmp
+import commands
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('namelist')
+
+@app.route('/list')
+def fn():
+    import os
+    fl = os.listdir(r'FILESYSTEM')
+    with open(r'templates/namelist', 'w+') as f:
+        for i in fl:
+            f.write(i+'<br />')
+    f.close()
+
+@app.route('/filesystem')
+def fs():
+    return render_template('filesystem')
+    
+def monitor_size():
+    from filecmp import cmp
+    result1 = cmp('FILESYSTEM/filesystem', 'templates/filesystem')
+    if result1 == bool(0):
+        commands.getoutput('python main.py runserver')
+
+def dur(op=None, clock=[time.time()]):
+    if op != None:
+        duration = time.time() - clock[0]
+        print ('%s Finished. Duration %.2f Seconds.') % op, duration
+        #print('{0} Finished. Duration {1:.2f} Seconds.'.format(op,duration))
+    clock[0] = time.time()
+    return dur
+        
+if __name__ == ('__main__'):
+    dur()
+    fn()
+    monitor_size()
+    app.run()
+    dur('restart    :')
+    #app.run(debug=True)
